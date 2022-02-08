41a0b8b93fb554f85d031c6c0bb6cef1bc71e3fe
duxxless <duxxless53@ya.ru>
2022-02-08 23:14:30 +0300

    Updated the code

--- CALayerApp/ViewController.swift
+++ CALayerApp/ViewController.swift
@@ -8,12 +8,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let positionAnimationLayer = CALayer()
+    let opacityAnimationLayer = CALayer()
+    let viewTwo = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let gradient = setGradient()
+        view.layer.insertSublayer(gradient, at: 0)
+        
+
+        
+    
+        
+        positionAnimationLayer.backgroundColor = UIColor.systemRed.cgColor
+        positionAnimationLayer.frame = CGRect(x: 50, y: 50, width: 100, height: 100)
+        positionAnimationLayer.cornerRadius = positionAnimationLayer.bounds.size.width/2
+        self.view.layer.addSublayer(positionAnimationLayer)
+        
+        viewTwo.frame = CGRect(x: view.layer.bounds.origin.x, y: view.layer.bounds.origin.y, width: 50, height: 50)
+                viewTwo.layer.cornerRadius = viewTwo.bounds.size.width/2
+                viewTwo.backgroundColor = .black
+                self.viewTwo.layer.addSublayer(opacityAnimationLayer)
+                self.view.addSubview(viewTwo)
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now()+1) {
+            self.setPositionAnimation()
+            self.viewTwo.layer.opacityAnimation(myDuration: 0.2)
+        }
+    
     }
-
-
+    
+    // CAGradientLayer
+    func setGradient() -> CAGradientLayer {
+        let gradient = CAGradientLayer()
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 0, y: 1)
+        gradient.colors = [UIColor.systemBlue.cgColor, UIColor.systemGreen.cgColor]
+        gradient.frame = self.view.bounds
+        
+        return gradient
+    }
+    
+    // CABasicAnimation - "position"
+    func setPositionAnimation() {
+        let animation = CABasicAnimation(keyPath: "position")
+        animation.fromValue = CGPoint(x: positionAnimationLayer.frame.origin.x + positionAnimationLayer.frame.size.width/2,
+                                      y: positionAnimationLayer.frame.origin.y + positionAnimationLayer.frame.size.height/2)
+        animation.toValue = CGPoint(x: 350 , y: 830)
+        animation.duration = 5
+        animation.fillMode = .forwards
+        animation.isRemovedOnCompletion = false
+    
+        positionAnimationLayer.add(animation, forKey: nil)
+    }
+    
 }
-
---
+++ ExtensionUIView.swift
@@ -0,0 +1,29 @@
+//
+//  ExtensionUIView.swift
+//  CALayerApp
+//
+//  Created by Duxxless on 08.02.2022.
+//
+
+import Foundation
+import UIKit
+
+extension CALayer {
+    
+    // CABasicAnimation - "opacity"
+    func opacityAnimation(myDuration: Double) {
+        
+        let label = CABasicAnimation(keyPath: "opacity")
+        
+        label.fromValue = 0
+        label.toValue = 1
+        label.duration = myDuration
+        label.fillMode = .forwards
+        label.isRemovedOnCompletion = false
+        label.autoreverses = true
+        label.repeatCount = 10
+        
+        self.add(label, forKey: nil)
+        
+    }
+}
